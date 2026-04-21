65e5d66b82eff16731a695c0fc633b740cb5ac82
Spriha <sprihasharma098@gmail.com>
2026-04-21 16:56:40 +0545

    Remove Matpplotlib_Line_plots_fixed.ipynb and fix.py

--- fix.py
+++
@@ -1,6 +0,0 @@
-import json, glob
-for f in glob.glob("*.ipynb"):
-    nb = json.load(open(f, encoding="utf-8"))
-    nb["metadata"]["kernelspec"] = nb["metadata"].get("kernelspec", {})
-    json.dump(nb, open(f, "w", encoding="utf-8"), indent=1)
-    print("Touched:", f)
